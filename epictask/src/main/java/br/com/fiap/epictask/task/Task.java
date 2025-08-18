2423f1a1ebd6d2d17d41426173908e7d3e62f420
Murilo-Capristo <murilocapristo@gmail.com>
2025-08-18 20:09:06 -0300

    feat: i18n | feat: ErrorValidation

--- epictask/src/main/java/br/com/fiap/epictask/task/Task.java
+++ epictask/src/main/java/br/com/fiap/epictask/task/Task.java
@@ -4,6 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,12 +19,20 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Task {
+    // Internationalization - i18n
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  Long id;
 
+    @NotBlank(message = "{task.title.notblank}")
     private String title;
+
+    @Size(min = 10, max = 255, message = "{task.description.size}")
     private String description;
+
+    @Min(1) @Max(100)
     private int  score;
+
+    @Min(1) @Max(100)
     private int status;
 }
--- epictask/src/main/java/br/com/fiap/epictask/task/TaskController.java
+++ epictask/src/main/java/br/com/fiap/epictask/task/TaskController.java
@@ -1,8 +1,10 @@
 package br.com.fiap.epictask.task;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,12 +31,15 @@ public class TaskController {
     }
 
     @GetMapping("/form")
-    public String form(){
+    public String form(Task task){
         return "form";
     }
 
     @PostMapping("/form")
-    public String creat(Task task, RedirectAttributes redirect){ //TO DO:  DTO
+    public String creat(@Valid Task task, BindingResult result, RedirectAttributes redirect){ //TO DO:  DTO
+        if (result.hasErrors()){
+            return "form";
+        }
         taskService.save(task);
         System.out.println(task);
         redirect.addFlashAttribute("message", "Tarefa cadastrada com sucesso!");
